d9a0850c289ab348338149b6b22d5dc7d326f8d1
lolotrgeek <lolotrgeek@gmail.com>
2020-03-20 12:12:07 -0500

    onclick open url

--- public/sw.js
+++ public/sw.js
@@ -8,7 +8,28 @@ self.addEventListener('activate', function (event) {
   event.waitUntil(clients.claim());
 });
 
-self.addEventListener('notificationclick', function (event) {
-  // Close the notification when it is clicked
-  event.notification.close();
+self.addEventListener('notificationclick', function(event) {  
+  console.log('On notification click: ', event.notification.tag);  
+  // Android doesn't close the notification when you click on it  
+  // See: http://crbug.com/463146  
+  // event.notification.close();
+
+  // This looks to see if the current is already open and  
+  // focuses if it is  
+  event.waitUntil(
+    clients.matchAll({  
+      type: "window"  
+    })
+    .then(function(clientList) {  
+      for (var i = 0; i < clientList.length; i++) {  
+        var client = clientList[i]; 
+        console.log(client.url) 
+        if (client.url === '/' && 'focus' in client)  
+          return client.focus();  
+      }  
+      if (clients.openWindow) {
+        return clients.openWindow(client.url);  
+      }
+    })
+  );
 });
