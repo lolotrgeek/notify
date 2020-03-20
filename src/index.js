af3125fa74c4f522a2d28ca866ac25e3b6d27c06
lolotrgeek <jon@simplyenable.com>
2020-03-20 08:38:57 -0500

    silence and stylize

--- src/index.js
+++ src/index.js
@@ -1,30 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-// ReactDOM.render(<App swRegistration={serviceWorker}  />, document.getElementById('root'));
-
-if ('serviceWorker' in navigator) {
+if ("serviceWorker" in navigator) {
   // if (
   //   process.env.NODE_ENV === 'production' &&
   //   ('https:' === location.protocol || location.host.match(/(localhost|127.0.0.1)/)) &&
   //   navigator.serviceWorker
   // ) {
-    navigator.serviceWorker.register('./sw.js').then(function (registration) {
+  navigator.serviceWorker.register("./sw.js").then(
+    function(registration) {
       // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      ReactDOM.render(<App swRegistration={registration} />, document.getElementById('root'));
-    }, function (err) {
+      console.log(
+        "ServiceWorker registration successful with scope: ",
+        registration.scope
+      );
+      ReactDOM.render(
+        <App swRegistration={registration} />,
+        document.getElementById("root")
+      );
+    },
+    function(err) {
       // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
+      console.log("ServiceWorker registration failed: ", err);
+    }
+  );
   // }
 }
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register()
+serviceWorker.register();
